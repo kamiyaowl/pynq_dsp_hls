dd793ceeb60e7fd6e550f9f0eea3a966cd9733f7
K <kamiyaowl@gmail.com>
2020-02-09 01:50:00 +0900

    v0.2を出した

--- pynq_dsp_hls/pynq_dsp_hls/script.tcl
+++ pynq_dsp_hls/pynq_dsp_hls/script.tcl
@@ -11,10 +11,10 @@ open_solution "pynq_dsp_hls"
 set_part {xc7z020-clg400-1} -tool vivado
 create_clock -period 8 -name default
 config_sdx -optimization_level none -target none
-config_export -description {Audio DSP Bypass Sample} -display_name Bypass -format ip_catalog -rtl verilog -vendor kamiyaowl -version 0.1 -vivado_optimization_level 2
+config_export -description {Audio DSP} -display_name {PYNQ DSP HLS} -format ip_catalog -rtl verilog -vendor kamiyaowl -version 0.2 -vivado_optimization_level 2
 set_clock_uncertainty 12.5%
 #source "./pynq_dsp_hls/pynq_dsp_hls/directives.tcl"
 csim_design
 csynth_design
 cosim_design -wave_debug -trace_level port -tool xsim
-export_design -rtl verilog -format ip_catalog -description "Audio DSP Bypass Sample" -vendor "kamiyaowl" -version "0.1" -display_name "Bypass"
+export_design -rtl verilog -format ip_catalog -description "Audio DSP" -vendor "kamiyaowl" -version "0.2" -display_name "PYNQ DSP HLS"
